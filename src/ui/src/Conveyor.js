80cb11f5d14b1de1f3cf00949f820f39eb34892a
Svetlin <svetlimladenov@gmail.com>
2021-09-16 00:58:09 +0300

    SAVEPOINT

--- src/ui/src/Conveyor.js
+++ src/ui/src/Conveyor.js
@@ -5,21 +5,52 @@ class Conveyor extends React.Component {
   constructor() {
     super();
     this.state = {
-      conveyor: ["extruder", "stamper", "oven", "box"],
+      conveyor: [{ name: "🍪" }, null, null, null],
+      isRunning: false,
+      step: 0,
     };
+
+    this.handleStartConveyor = this.handleStartConveyor.bind(this);
+  }
+
+  handleStartConveyor() {
+    this.setState({ isRunning: true });
+
+    let interval = setInterval(() => {
+      console.log(this.state.step);
+      if (this.state.step === 4) {
+        clearInterval(interval);
+        return;
+      }
+
+      this.setState(({ step, conveyor }) => {
+        let updateConveyor = Array(4).fill(null);
+        updateConveyor[step] = { name: "🍪" };
+
+        return {
+          step: step + 1,
+          conveyor: updateConveyor,
+        };
+      });
+    }, 1000);
   }
 
   render() {
     const { conveyor } = this.state;
     return (
-      <div className="conveyor-wrapper">
-        {conveyor.map((element, idx) => {
-          return (
-            <div className={`${element} element`} key={idx}>
-              {element}
-            </div>
-          );
-        })}
+      <div>
+        <div className="conveyor-wrapper">
+          {conveyor.map((element, idx) => {
+            const displayElement = element ? element.name : "";
+
+            return (
+              <div className={`${element} element`} key={idx}>
+                {displayElement}
+              </div>
+            );
+          })}
+        </div>
+        <button onClick={this.handleStartConveyor}>Start Conveyor</button>
       </div>
     );
   }
