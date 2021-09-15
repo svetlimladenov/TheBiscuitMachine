df10e6bc1b98f52e4d6bb468434462d25384f938
Svetlin <svetlimladenov@gmail.com>
2021-09-15 14:48:07 +0300

    Test the React web socket connection

--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -1,7 +1,46 @@
+import React from 'react';
 import './App.css';
+import * as signalR from "@microsoft/signalr";
 
-function App() {
-  return <h1>The Biscuit Machine</h1>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: ["test", "jaja"]
+    }
+  }
+
+  componentDidMount() {
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl("https://localhost:5001/machinehub")
+      .build();
+
+
+    connection.on("ReceiveMessage", (message) => {
+      this.setState((prevState) => {
+        return {
+          messages: [...prevState.messages, message]
+        }
+      })
+    });
+
+    connection.start().then((x) => {
+      console.log("Connected!!!");
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Messages: </h3>
+        <ul>
+          {this.state.messages.map((msg, idx) => {
+            return <li key={idx}>{msg}</li>
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
