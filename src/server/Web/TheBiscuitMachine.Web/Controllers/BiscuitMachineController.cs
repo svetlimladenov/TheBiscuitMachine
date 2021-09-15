2f000dd538711bda92b8fbc4b55ed1245aa76fb0
Svetlin <svetlimladenov@gmail.com>
2021-09-15 19:57:07 +0300

    Rename the Biscuit Controller

--- src/server/Web/TheBiscuitMachine.Web/Controllers/BiscuitMachineController.cs
+++ src/server/Web/TheBiscuitMachine.Web/Controllers/BiscuitMachineController.cs
@@ -1,25 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MassTransit;
-using MassTransit.Mediator;
 using Microsoft.AspNetCore.Mvc;
 using TheBiscuitMachine.Web.Contracts;
 
 namespace TheBiscuitMachine.Web.Controllers
 {
     [ApiController]
-    [Route("Biscuit")]
-    public class BiscuitController : ControllerBase
+    [Route("Machine")]
+    public class BiscuitMachineController : ControllerBase
     {
         private readonly IBus mediator;
 
-        public BiscuitController(IBus mediator)
+        public BiscuitMachineController(IBus mediator)
         {
             this.mediator = mediator;
         }
 
+        [Route("Start")]
         [HttpPost]
         public async Task<IActionResult> Start(string userId)
         {
