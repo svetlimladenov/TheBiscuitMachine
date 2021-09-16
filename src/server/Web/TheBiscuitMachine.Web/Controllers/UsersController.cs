a2dc48a6cf0142a198833d040f09b533f7051548
Svetlin <svetlimladenov@gmail.com>
2021-09-16 20:42:27 +0300

    Add empty users controller

---
+++ src/server/Web/TheBiscuitMachine.Web/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheBiscuitMachine.Web.Controllers
+{
+    [ApiController]
+    [Route("Users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost("Login")]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+
+        [HttpPost("Register")]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+    }
+}
