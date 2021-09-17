ba2f7832ae23c0971548ab43c41897e3bb5fcb40
Svetlin <svetlimladenov@gmail.com>
2021-09-17 13:01:11 +0300

    Add machines name property

--- src/server/Data/TheBiscuitMachine.Data/Models/Machine.cs
+++ src/server/Data/TheBiscuitMachine.Data/Models/Machine.cs
@@ -4,6 +4,8 @@
     {
         public string Id { get; set; }
 
+        public string Name { get; set; }
+
         public string UserId { get; set; }
 
         public User User { get; set; }
---
+++ src/server/Infrastructure/TheBiscuitMachine.Infrastructure/Migrations/20210917093210_AddMachineName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheBiscuitMachine.Infrastructure.Migrations
+{
+    public partial class AddMachineName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Machines",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Machines");
+        }
+    }
+}
