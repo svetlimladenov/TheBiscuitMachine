4c79d793d1779b3b16f32b65324195423a14a3bd
Svetlin <svetlimladenov@gmail.com>
2021-09-17 18:19:53 +0300

    Rework the unit tests to work the the inmemory db, and to dispose harness and context

--- src/server/Tests/TheBiscuitMachine.Tests/Application/Consumers/RegisterConsumerTests.cs
+++ src/server/Tests/TheBiscuitMachine.Tests/Application/Consumers/RegisterConsumerTests.cs
@@ -2,25 +2,28 @@
 using System.Threading.Tasks;
 using MassTransit;
 using MassTransit.Testing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Shouldly;
+using TheBiscuitMachine.Application.Common.Interfaces;
 using TheBiscuitMachine.Application.Consumers;
 using TheBiscuitMachine.Application.Contracts;
 using TheBiscuitMachine.Data.Models;
+using TheBiscuitMachine.Infrastructure;
 using Xunit;
 
 namespace TheBiscuitMachine.Tests.Application.Consumers
 {
-    public class RegisterConsumerTests
+    public class RegisterConsumerTests : IAsyncLifetime
     {
         private readonly IServiceProvider serviceProvider;
         private readonly InMemoryTestHarness harness;
-        private readonly Mock<IUserService> userServiceMock;
+        private readonly TheBiscuitMachineContext context;
 
         public RegisterConsumerTests()
         {
-            userServiceMock = new Mock<IUserService>();
+            context = DbContextFactory.Create();
 
             this.serviceProvider = new ServiceCollection()
                 .AddMassTransitInMemoryTestHarness(cfg =>
@@ -28,7 +31,7 @@ namespace TheBiscuitMachine.Tests.Application.Consumers
                     cfg.AddConsumer<RegisterConsumer>();
                     cfg.AddConsumerTestHarness<RegisterConsumer>();
                 })
-                .AddScoped(x => userServiceMock.Object)
+                .AddSingleton<IDbContext>(x => context)
                 .BuildServiceProvider(true);
 
             this.harness = this.serviceProvider.GetRequiredService<InMemoryTestHarness>();
@@ -42,28 +45,19 @@ namespace TheBiscuitMachine.Tests.Application.Consumers
             const string email = "yasuo@riot.com";
             string machineName = $"{username}'s Biscuit Machine";
 
-            this.userServiceMock.Setup(x => x.GetUserAsync(It.IsAny<string>())).ReturnsAsync(value: null);
+            var bus = this.serviceProvider.GetRequiredService<IBus>();
 
-            await harness.Start();
-            try
-            {
-                var bus = this.serviceProvider.GetRequiredService<IBus>();
+            IRequestClient<RegisterRequest> client = bus.CreateRequestClient<RegisterRequest>();
 
-                IRequestClient<RegisterRequest> client = bus.CreateRequestClient<RegisterRequest>();
+            // Act
+            var response = await client.GetResponse<RegisterResponse>(new { Username = username, Email = email });
 
-                // Act
-                var response = await client.GetResponse<RegisterResponse>(new { Username = username, Email = email });
-
-                // Assert
-                response.Message.Success.ShouldBeTrue();
-                Assert.True(await harness.Consumed.Any<RegisterResponse>());
-                this.userServiceMock.Verify(x => x.AddAsync(It.Is<User>(x => x.Username == username && x.Email == email && x.Machine.Name == machineName)));
-                this.userServiceMock.Verify(x => x.SaveChangesAsync());
-            }
-            finally
-            {
-                await harness.Stop();
-            }
+            // Assert
+            response.Message.Success.ShouldBeTrue();
+            var user = await context.Users.FirstOrDefaultAsync();
+            user.Username.ShouldBe(username);
+            user.Machine.Name.ShouldBe(machineName);
+            Assert.True(await harness.Consumed.Any<RegisterResponse>());
         }
 
         [Fact]
@@ -72,27 +66,38 @@ namespace TheBiscuitMachine.Tests.Application.Consumers
             // Arrange
             const string username = "Yasuo";
             const string email = "yasuo@riot.com";
-            this.userServiceMock.Setup(x => x.GetUserAsync(It.IsAny<string>())).ReturnsAsync(new User());
 
+            AddUser(username, email);
+
+            var bus = this.serviceProvider.GetRequiredService<IBus>();
+
+            IRequestClient<RegisterRequest> client = bus.CreateRequestClient<RegisterRequest>();
+
+            // Act
+            var response = await client.GetResponse<RegisterResponse>(new { Username = username, Email = email });
+
+            // Assert
+            response.Message.Success.ShouldBeFalse();
+            response.Message.ValidationErrors.ShouldNotBeEmpty();
+            context.Users.ShouldHaveSingleItem();
+            Assert.True(await harness.Consumed.Any<RegisterResponse>());
+        }
+
+        private void AddUser(string username, string email)
+        {
+            context.Users.Add(new User() { Username = username, Email = email });
+            context.SaveChanges();
+        }
+
+        public async Task InitializeAsync()
+        {
             await harness.Start();
-            try
-            {
-                var bus = this.serviceProvider.GetRequiredService<IBus>();
+        }
 
-                IRequestClient<RegisterRequest> client = bus.CreateRequestClient<RegisterRequest>();
-
-                // Act
-                var response = await client.GetResponse<RegisterResponse>(new { Username = username, Email = email });
-
-                // Assert
-                response.Message.Success.ShouldBeFalse();
-                response.Message.ValidationErrors.ShouldNotBeEmpty();
-                Assert.True(await harness.Consumed.Any<RegisterResponse>());
-            }
-            finally
-            {
-                await harness.Stop();
-            }
+        public async Task DisposeAsync()
+        {
+            DbContextFactory.Destroy(context);
+            await harness.Stop();
         }
     }
 }
---
+++ src/server/Tests/TheBiscuitMachine.Tests/DbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TheBiscuitMachine.Infrastructure;
+
+namespace TheBiscuitMachine.Tests
+{
+    public static class DbContextFactory
+    {
+        public static TheBiscuitMachineContext Create()
+        {
+            var options = new DbContextOptionsBuilder<TheBiscuitMachineContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new TheBiscuitMachineContext(options);
+
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+
+        public static void Destroy(TheBiscuitMachineContext context)
+        {
+            context.Database.EnsureDeleted();
+
+            context.Dispose();
+        }
+    }
+}
