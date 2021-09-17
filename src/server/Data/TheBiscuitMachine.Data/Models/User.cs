413f60107da7b1d3e997bddbb7454d5001c311b1
Svetlin <svetlimladenov@gmail.com>
2021-09-17 12:02:32 +0300

    Add some domain entities

---
+++ src/server/Data/TheBiscuitMachine.Data/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TheBiscuitMachine.Data.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string MachineId { get; set; }
+
+        public Machine Machine { get; set; }
+
+        public List<BiscuitPackage> BiscuitPackages { get; set; }
+    }
+}
