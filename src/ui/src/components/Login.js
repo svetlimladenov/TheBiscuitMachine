9ca5c0d5e1eb8956dc04b80333e5822aadf9e4a4
Svetlin <svetlimladenov@gmail.com>
2021-09-18 19:43:16 +0300

    Add some basic filed logic for a login component

---
+++ src/ui/src/components/Login.js
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+import Field from "./Field";
+
+const Login = ({ onSubmit }) => {
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    onSubmit(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Field ref={usernameRef} label="Username: " type="text" />
+      <Field ref={passwordRef} label="Password: " type="password" />
+      <div>
+        <button type="submit">Login</button>
+      </div>
+    </form>
+  );
+};
+
+export default Login;
