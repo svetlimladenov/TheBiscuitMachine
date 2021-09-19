813c66cb2a34ab15b5be799833ac21023d08628b
Svetlin <svetlimladenov@gmail.com>
2021-09-19 16:00:57 +0300

    Imrpove the fetch util

--- src/ui/src/shared/fetch.js
+++ src/ui/src/shared/fetch.js
@@ -2,23 +2,30 @@ import axios from "axios";
 
 export const defaults = {
   signalRUrl: "https://localhost:5001",
-  apiUrl: "https://localhost:5001/",
+  apiUrl: "https://localhost:5001",
 };
 
-const fetch = (method, url) => {
+const fetch = (method, url, data) => {
   return new Promise((resolve, reject) => {
     axios({
       url: `${defaults.apiUrl}${url}`,
       method,
+      data: method !== "GET" ? data : undefined,
     }).then(
       (res) => {
         resolve(res);
       },
-      (err) => {
-        console.error(err);
+      (error) => {
+        const validationErrors = error.response.data.errors;
+        reject(validationErrors);
       }
     );
   });
 };
 
-export default fetch;
+const api = {
+  get: (...args) => fetch("GET", ...args),
+  post: (...args) => fetch("POST", ...args),
+};
+
+export default api;
