a33a4f0926f291c9b0a723bf95bf2796316eb5d9
Svetlin <svetlimladenov@gmail.com>
2021-09-19 18:51:36 +0300

    Remove trash logs

--- src/server/Application/TheBiscuitMachine.Application/Consumers/NotifyOvenHeatedConsumer.cs
+++ src/server/Application/TheBiscuitMachine.Application/Consumers/NotifyOvenHeatedConsumer.cs
@@ -8,18 +8,15 @@ namespace TheBiscuitMachine.Application.Consumers
 {
     public class NotifyOvenHeatedConsumer : IConsumer<NotifyOvenHeated>
     {
-        private readonly ILogger<NotifyOvenHeatedConsumer> logger;
         private readonly IBiscuitMachineNotificator biscuitMachineNotificator;
 
         public NotifyOvenHeatedConsumer(ILogger<NotifyOvenHeatedConsumer> logger, IBiscuitMachineNotificator biscuitMachineNotificator)
         {
-            this.logger = logger;
             this.biscuitMachineNotificator = biscuitMachineNotificator;
         }
 
         public async Task Consume(ConsumeContext<NotifyOvenHeated> context)
         {
-            logger.LogError("OVEN HEATED!!!");
             await biscuitMachineNotificator.NotifyOvenHeated(context.Message.UserId);
         }
     }
--- src/server/Application/TheBiscuitMachine.Application/Consumers/NotifyOvenOverheatedConsumer.cs
+++ src/server/Application/TheBiscuitMachine.Application/Consumers/NotifyOvenOverheatedConsumer.cs
@@ -8,18 +8,15 @@ namespace TheBiscuitMachine.Application.Consumers
 {
     public class NotifyOvenOverheatedConsumer : IConsumer<NotifyOvenOverheated>
     {
-        private readonly ILogger<NotifyOvenOverheatedConsumer> logger;
         private readonly IBiscuitMachineNotificator biscuitMachineNotificator;
 
         public NotifyOvenOverheatedConsumer(ILogger<NotifyOvenOverheatedConsumer> logger, IBiscuitMachineNotificator biscuitMachineNotificator)
         {
-            this.logger = logger;
             this.biscuitMachineNotificator = biscuitMachineNotificator;
         }
 
         public async Task Consume(ConsumeContext<NotifyOvenOverheated> context)
         {
-            this.logger.LogError("OVERHEATING !!!!");
             await this.biscuitMachineNotificator.NotifyOvenOverheated(context.Message.UserId);
         }
     }
