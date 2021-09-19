98e3326df677172da000bb744f865f539f261588
Svetlin <svetlimladenov@gmail.com>
2021-09-19 18:11:42 +0300

    Convert the App.js to a function component

--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -1,66 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import api from "./shared/fetch";
 
 import Routing from "./components/Routing";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      register: {
-        error: "",
-      },
-      user: {
-        isLoggedIn: false,
-        id: "",
-      },
-    };
-  }
+export default function App() {
+  const [user, setUser] = useState({ isLoggedIn: false, id: "" });
 
-  login = ({ data }) => {
-    this.setState({
-      user: {
-        id: data,
-        isLoggedIn: true,
-      },
+  const login = ({ data }) => {
+    setUser({
+      id: data,
+      isLoggedIn: true,
     });
   };
 
-  handleLoginSubmit = (body) => {
-    api.post("/Users/Login", body).then(this.login);
+  const handleLoginSubmit = (body) => {
+    api.post("/Users/Login", body).then(login);
   };
 
-  handleRegisterSubmit = (body) => {
-    api.post("/Users/Register", body).then(this.login);
+  const handleRegisterSubmit = (body) => {
+    api.post("/Users/Register", body).then(login);
   };
 
-  render() {
-    const isLoggedIn = this.state.user.isLoggedIn;
+  const links = user.isLoggedIn
+    ? Routing.renderLoggedInLinks()
+    : Routing.renderLoginLinks();
 
-    const links = isLoggedIn
-      ? Routing.renderLoggedInLinks()
-      : Routing.renderLoginLinks();
+  const components = user.isLoggedIn
+    ? Routing.renderLoggedInComponents(user)
+    : Routing.renderLoginComponents(handleLoginSubmit, handleRegisterSubmit);
 
-    const components = isLoggedIn
-      ? Routing.renderLoggedInComponents(this.state.user)
-      : Routing.renderLoginComponents(
-          this.handleLoginSubmit,
-          this.handleRegisterSubmit,
-          this.state.register.error
-        );
-
-    return (
-      <Router>
-        <div>
-          <nav>
-            <ul>{links}</ul>
-          </nav>
-          <Switch>{components}</Switch>
-        </div>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>{links}</ul>
+        </nav>
+        <Switch>{components}</Switch>
+      </div>
+    </Router>
+  );
 }
-
-export default App;
