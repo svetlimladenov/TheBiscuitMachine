45b952a10c8b51be7c67ede372997654645d9d52
Svetlin <svetlimladenov@gmail.com>
2021-09-19 13:00:22 +0300

    Add method for verifying password hashes

---
+++ src/server/Application/TheBiscuitMachine.Application/Common/Interfaces/IBiscuitMachinePasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace TheBiscuitMachine.Application.Common.Interfaces
+{
+    public interface IBiscuitMachinePasswordHasher
+    {
+        string HashPassword(string password);
+
+        bool VerifyHashedPassword(string hashedPassword, string providerPassword);
+    }
+}
---
+++ src/server/Web/TheBiscuitMachine.Web/Services/BiscuitMachinePasswordHasher.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using TheBiscuitMachine.Application.Common.Interfaces;
+
+namespace TheBiscuitMachine.Web.Services
+{
+    public class BiscuitMachinePasswordHasher : IBiscuitMachinePasswordHasher
+    {
+        private readonly RandomNumberGenerator randomNumberGenerator;
+
+        public BiscuitMachinePasswordHasher()
+        {
+            randomNumberGenerator = RandomNumberGenerator.Create();
+        }
+
+        public string HashPassword(string password)
+        {
+            var hashed = Hash(password, this.randomNumberGenerator);
+            return Convert.ToBase64String(hashed);
+        }
+
+        // Based on AspNetCores Password Hasher
+        // https://github.com/dotnet/AspNetCore/blob/main/src/Identity/Extensions.Core/src/PasswordHasher.cs
+        private static byte[] Hash(string password, RandomNumberGenerator randomNumberGenerator)
+        {
+            const KeyDerivationPrf Pbkdf2Prf = KeyDerivationPrf.HMACSHA1; // default for Rfc2898DeriveBytes
+            const int Pbkdf2IterCount = 1000; // default for Rfc2898DeriveBytes
+            const int Pbkdf2SubkeyLength = 256 / 8; // 256 bits
+            const int SaltSize = 128 / 8; // 128 bits
+
+            // Produce a version 2 (see comment above) text hash.
+            byte[] salt = new byte[SaltSize];
+            randomNumberGenerator.GetBytes(salt);
+            byte[] subkey = KeyDerivation.Pbkdf2(password, salt, Pbkdf2Prf, Pbkdf2IterCount, Pbkdf2SubkeyLength);
+
+            var outputBytes = new byte[1 + SaltSize + Pbkdf2SubkeyLength];
+            outputBytes[0] = 0x00; // format marker
+            Buffer.BlockCopy(salt, 0, outputBytes, 1, SaltSize);
+            Buffer.BlockCopy(subkey, 0, outputBytes, 1 + SaltSize, Pbkdf2SubkeyLength);
+            return outputBytes;
+        }
+
+        public bool VerifyHashedPassword(string hashedPassword, string providerPassword)
+        {
+            byte[] decodedHashedPassword = Convert.FromBase64String(hashedPassword);
+            if (decodedHashedPassword.Length == 0)
+            {
+                return false;
+            }
+
+            return VerifyHashedPassword(decodedHashedPassword, providerPassword);
+        }
+
+        private static bool VerifyHashedPassword(byte[] hashedPassword, string password)
+        {
+            const KeyDerivationPrf Pbkdf2Prf = KeyDerivationPrf.HMACSHA1; // default for Rfc2898DeriveBytes
+            const int Pbkdf2IterCount = 1000; // default for Rfc2898DeriveBytes
+            const int Pbkdf2SubkeyLength = 256 / 8; // 256 bits
+            const int SaltSize = 128 / 8; // 128 bits
+
+            // We know ahead of time the exact length of a valid hashed password payload.
+            if (hashedPassword.Length != 1 + SaltSize + Pbkdf2SubkeyLength)
+            {
+                return false; // bad size
+            }
+
+            byte[] salt = new byte[SaltSize];
+            Buffer.BlockCopy(hashedPassword, 1, salt, 0, salt.Length);
+
+            byte[] expectedSubkey = new byte[Pbkdf2SubkeyLength];
+            Buffer.BlockCopy(hashedPassword, 1 + salt.Length, expectedSubkey, 0, expectedSubkey.Length);
+
+            // Hash the incoming password and verify it
+            byte[] actualSubkey = KeyDerivation.Pbkdf2(password, salt, Pbkdf2Prf, Pbkdf2IterCount, Pbkdf2SubkeyLength);
+
+            return CryptographicOperations.FixedTimeEquals(actualSubkey, expectedSubkey);
+        }
+    }
+}
