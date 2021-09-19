9812baa111865e91410907f0c89da3c6de6af55b
Svetlin <svetlimladenov@gmail.com>
2021-09-19 13:00:44 +0300

    Unit test the password hasher

---
+++ src/server/Tests/TheBiscuitMachine.Tests/Web/PasswordHasherTests.cs
@@ -0,0 +1,34 @@
+﻿using TheBiscuitMachine.Web.Services;
+using Xunit;
+
+namespace TheBiscuitMachine.Tests.Web
+{
+    public class PasswordHasherTests
+    {
+        [Fact]
+        public void VerifyHashedPassword_Should_ReturnTrue_WhenPasswordIsCorrect()
+        {
+            const string password = "VeryStrongPassword1234";
+
+            var hasher = new BiscuitMachinePasswordHasher();
+
+            var savedPasswordHash = hasher.HashPassword(password);
+
+            var result = hasher.VerifyHashedPassword(savedPasswordHash, password);
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("VeryStrongPassword1234", "VeryStringWrongPassword")]
+        [InlineData("aaaaaaaaA", "aaaaaaaaa")]
+        public void VerifyHashedPassword_Should_ReturnFalse_WhenPasswordIsNOTCorrect(string password, string wrongPassword)
+        {
+            var hasher = new BiscuitMachinePasswordHasher();
+
+            var savedPasswordHash = hasher.HashPassword(password);
+
+            var result = hasher.VerifyHashedPassword(savedPasswordHash, wrongPassword);
+            Assert.False(result);
+        }
+    }
+}
