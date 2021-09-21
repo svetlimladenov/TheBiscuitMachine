f922e6ee639a797af083ae6d2a1140d9a5d6bff3
Svetlin <svetlimladenov@gmail.com>
2021-09-21 15:59:59 +0300

    Add more unit tests covering the new machine configurations

--- src/server/Tests/TheBiscuitMachine.Tests/Application/Consumers/RegisterConsumerTests.cs
+++ src/server/Tests/TheBiscuitMachine.Tests/Application/Consumers/RegisterConsumerTests.cs
@@ -10,6 +10,7 @@ using Shouldly;
 using TheBiscuitMachine.Application.Common.Interfaces;
 using TheBiscuitMachine.Application.Consumers;
 using TheBiscuitMachine.Application.Contracts;
+using TheBiscuitMachine.Data.Common;
 using TheBiscuitMachine.Data.Models;
 using TheBiscuitMachine.Infrastructure;
 using TheBiscuitMachine.Tests.Common;
@@ -20,14 +21,27 @@ namespace TheBiscuitMachine.Tests.Application.Consumers
 {
     public class RegisterConsumerTests : IAsyncLifetime
     {
+        private readonly Mock<IBiscuitMachineConfigurator> biscuitMachineConfigurator = new Mock<IBiscuitMachineConfigurator>();
         private readonly Mock<IBiscuitMachinePasswordHasher> passwordHasherMock = new Mock<IBiscuitMachinePasswordHasher>();
         private readonly IServiceProvider serviceProvider;
         private readonly InMemoryTestHarness harness;
         private readonly TheBiscuitMachineContext context;
 
+        private MachineConfiguration defaultMachineConfiguration = new MachineConfiguration()
+        {
+            Pulse = 2,
+            OvenHeatingDuration = TimeSpan.FromSeconds(20),
+            OvenOverheatingDuration = TimeSpan.FromSeconds(100),
+            OvenColdDuration = TimeSpan.FromSeconds(120)
+        };
+
         public RegisterConsumerTests(ITestOutputHelper testOutputHelper)
         {
             context = DbContextFactory.Create();
+
+            biscuitMachineConfigurator
+                .Setup(x => x.GetDefaultMachineConfig())
+                .Returns(defaultMachineConfiguration);
 
             this.serviceProvider = new ServiceCollection()
                 .AddMassTransitInMemoryTestHarness(cfg =>
@@ -38,13 +52,14 @@ namespace TheBiscuitMachine.Tests.Application.Consumers
                 .AddSingleton<ILoggerFactory>(provider => new TestOutputLoggerFactory(true, testOutputHelper))
                 .AddSingleton<IDbContext>(x => context)
                 .AddScoped(x => passwordHasherMock.Object)
+                .AddSingleton(x => biscuitMachineConfigurator.Object)
                 .BuildServiceProvider(true);
 
             this.harness = this.serviceProvider.GetRequiredService<InMemoryTestHarness>();
         }
 
         [Fact]
-        public async Task ShouldReturnSuccess_When_UserNotAlreadyRegistered()
+        public async Task ShouldReturnSuccessAndCreateUserAndMachine_When_UserNotAlreadyRegistered()
         {
             // Arrange
             const string username = "Yasuo";
@@ -64,6 +79,12 @@ namespace TheBiscuitMachine.Tests.Application.Consumers
             var user = await context.Users.FirstOrDefaultAsync();
             user.Username.ShouldBe(username);
             user.Machine.Name.ShouldBe(machineName);
+
+            user.Machine.Pulse.ShouldBe(this.defaultMachineConfiguration.Pulse);
+            user.Machine.OvenHeatingDurationTicks.ShouldBe(this.defaultMachineConfiguration.OvenHeatingDuration.Ticks);
+            user.Machine.OvenOverheatingDurationTicks.ShouldBe(this.defaultMachineConfiguration.OvenOverheatingDuration.Ticks);
+            user.Machine.OvenColdDurationTicks.ShouldBe(this.defaultMachineConfiguration.OvenColdDuration.Ticks);
+
             Assert.True(await harness.Consumed.Any<RegisterResponse>());
         }
 
@@ -89,6 +110,58 @@ namespace TheBiscuitMachine.Tests.Application.Consumers
             response.Message.ValidationErrors.ShouldNotBeEmpty();
             context.Users.ShouldHaveSingleItem();
             Assert.True(await harness.Consumed.Any<RegisterResponse>());
+        }
+
+        [Fact]
+        public async Task ShouldNOTAddUsers_When_TheMachineConfiguratorThrows()
+        {
+            // Arrange
+            const string username = "Yasuo";
+            const string email = "yasuo@riot.com";
+            const string password = "password";
+            const string exceptionMessage = "HellOfALife!";
+
+            this.biscuitMachineConfigurator.Setup(x => x.GetDefaultMachineConfig()).Throws(new Exception(exceptionMessage));
+
+            var bus = this.serviceProvider.GetRequiredService<IBus>();
+            IRequestClient<RegisterRequest> client = bus.CreateRequestClient<RegisterRequest>();
+
+            // Act
+            try
+            {
+                var response = await client.GetResponse<RegisterResponse>(new { Username = username, Email = email, Password = password });
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ShouldContain(exceptionMessage);
+            }
+
+            context.Users.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public async Task ShouldNOTAddUsers_When_PasswordHasherFails()
+        {
+            // Arrange
+            const string username = "Yasuo";
+            const string email = "yasuo@riot.com";
+            const string password = "password";
+            const string exceptionMessage = "POWER";
+
+            this.passwordHasherMock.Setup(x => x.HashPassword(password)).Throws(new Exception(exceptionMessage));
+
+            var bus = this.serviceProvider.GetRequiredService<IBus>();
+            IRequestClient<RegisterRequest> client = bus.CreateRequestClient<RegisterRequest>();
+
+            // Act
+            try
+            {
+                var response = await client.GetResponse<RegisterResponse>(new { Username = username, Email = email, Password = password });
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ShouldContain(exceptionMessage);
+            }
         }
 
         private void AddUser(string username, string email, string passwordHash)
