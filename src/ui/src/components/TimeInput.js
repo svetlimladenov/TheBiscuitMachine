9899769dc6eb555fcefcac09075510465104fb52
Svetlin <svetlimladenov@gmail.com>
2021-09-23 13:43:06 +0300

    Wrap the lib picker into mine component

---
+++ src/ui/src/components/TimeInput.js
@@ -0,0 +1,26 @@
+import React from "react";
+import TimeField from "react-simple-timefield";
+
+const TimeInput = React.forwardRef(({ label, time }, ref) => {
+  return (
+    <div className="time-input">
+      <label>{label}</label>
+      <TimeField
+        showSeconds
+        value={time}
+        inputRef={ref}
+        style={{
+          border: "2px solid #666",
+          width: "100px",
+          padding: "5px 8px",
+          borderRadius: "3px",
+          backgroundColor: "#140f0f36",
+          color: "white",
+          outline: "none",
+        }}
+      />
+    </div>
+  );
+});
+
+export default TimeInput;
